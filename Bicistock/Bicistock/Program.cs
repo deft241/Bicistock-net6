02215b599ffabcfa94097c797b2559e4494f6dc8
deft241 <aaronclase24@gmail.com>
2023-06-05 00:46:33 +0200

    Changed connectionstring to AWS

--- Bicistock/Bicistock/Program.cs
+++ Bicistock/Bicistock/Program.cs
@@ -15,6 +15,7 @@ namespace Bicistock
                 {
                     webBuilder.UseStartup<Startup>()
                     .UseUrls("http://0.0.0.0:80");
+                    //.UseUrls("http://192.168.0.17:8080");
                 });
     }
 }
